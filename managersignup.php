8f74b6fe965bdc973146bf67dbe3658716109718
krisduy <nguyenthai0871@gmail.com>
2025-09-21 03:28:04 +0700

    sửa trang đăng kí admin

--- managersignup.php
+++ managersignup.php
@@ -3,7 +3,7 @@
 <html>
 
   <head>
-    <title> Manager Signup | Food Exploria </title>
+    <title> Đăng Kí Admin | HUYFOOD </title>
   </head>
 
   <link rel="stylesheet" type = "text/css" href ="css/managersignup.css">
@@ -13,63 +13,39 @@
 
   <body>
 
-  <!--Back to top button..................................................................................-->
-    <button onclick="topFunction()" id="myBtn" title="Go to top">
-      <span class="glyphicon glyphicon-chevron-up"></span>
-    </button>
-  <!--Javacript for back to top button....................................................................-->
-    <script type="text/javascript">
-      window.onscroll = function()
-      {
-        scrollFunction()
-      };
-
-      function scrollFunction(){
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById("myBtn").style.display = "block";
-        } else {
-          document.getElementById("myBtn").style.display = "none";
-        }
-      }
-
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
-    </script>
 
     <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
-            <span class="sr-only">Toggle navigation</span>
+            <span class="sr-only">thanh quản lí</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Food Exploria</a>
+          <a class="navbar-brand" href="index.php">HUYFOOD</a>
         </div>
 
         <div class="collapse navbar-collapse " id="myNavbar">
           <ul class="nav navbar-nav">
-            <li  ><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About</a></li>
-            <li><a href="contactus.php">Contact Us</a></li>
+            <li  ><a href="index.php">TRANG CHỦ</a></li>
+            <li><a href="aboutus.php">VỀ CHÚNG TÔI</a></li>
+            <li><a href="contactus.php">LIÊN HỆ</a></li>
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Đăng Kí <span class="caret"></span> </a>
                 <ul class="dropdown-menu">
-              <li> <a href="customersignup.php"> User Sign-up</a></li>
-              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+              <li> <a href="customersignup.php"> User Đăng Kí</a></li>
+              <li> <a href="managersignup.php"> Admin Đăng Kí</a></li>
            
             </ul>
             </li>
 
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Đăng Nhập <span class="caret"></span></a>
               <ul class="dropdown-menu">
-              <li> <a href="customerlogin.php"> User Login</a></li>
-              <li> <a href="managerlogin.php"> Manager Login</a></li>
+              <li> <a href="customerlogin.php"> User Đăng Nhập</a></li>
+              <li> <a href="managerlogin.php"> Admin Đăng Nhập</a></li>
             
             </ul>
             </li>
@@ -81,9 +57,9 @@
 
     <div class="container">
     <div class="jumbotron">
-     <h1>Hi Manager, <br> Welcome to <span class="edit"> FoodExploria </span></h1>
+     <h1>Xin chào Admin, <br> Chào mừng đến với <span class="edit"> HUYFOOD </span></h1>
      <br>
-   <p>Get started by creating your account</p>
+   <p>Tạo tài khoản mới</p>
     </div>
     </div>
 
@@ -92,14 +68,14 @@
     <div class="container" style="margin-top: 4%; margin-bottom: 2%;">
       <div class="col-md-5 col-md-offset-4">
       <div class="panel panel-primary">
-        <div class="panel-heading"> Create Account </div>
+        <div class="panel-heading"> Tạo tài khoản </div>
         <div class="panel-body">
           
         <form role="form" action="manager_registered_success.php" method="POST">
          
           <div class="row">
           <div class="form-group col-xs-12">
-            <label for="fullname"><span class="text-danger" style="margin-right: 5px;">*</span> Full Name: </label>
+            <label for="fullname"><span class="text-danger" style="margin-right: 5px;">*</span> Họ và tên: </label>
             <div class="input-group">
               <input class="form-control" id="fullname" type="text" name="fullname" placeholder="Your Full Name" required="" autofocus="">
               <span class="input-group-btn">
@@ -138,7 +114,7 @@
 
         <div class="row">
           <div class="form-group col-xs-12">
-            <label for="contact"><span class="text-danger" style="margin-right: 5px;">*</span> Contact: </label>
+            <label for="contact"><span class="text-danger" style="margin-right: 5px;">*</span> Số điện thoại: </label>
             <div class="input-group">
               <input class="form-control" id="contact" type="text" name="contact" placeholder="Contact" required="">
               <span class="input-group-btn">
@@ -151,7 +127,7 @@
 
         <div class="row">
           <div class="form-group col-xs-12">
-            <label for="address"><span class="text-danger" style="margin-right: 5px;">*</span> Address: </label>
+            <label for="address"><span class="text-danger" style="margin-right: 5px;">*</span> Địa chỉ: </label>
             <div class="input-group">
               <input class="form-control" id="address" type="text" name="address" placeholder="Address" required="">
               <span class="input-group-btn">
@@ -179,12 +155,12 @@
 
         <div class="row">
           <div class="form-group col-xs-4">
-              <button class="btn btn-primary" type="submit">Submit</button>
+              <button class="btn btn-primary" type="submit">Đăng Kí</button>
           </div>
 
         </div>
         <label style="margin-left: 5px;">or</label> <br>
-       <label style="margin-left: 5px;"><a href="managerlogin.php">Have an account? Login.</a></label>
+       <label style="margin-left: 5px;"><a href="managerlogin.php">Đã có tài khoản? Đăng Nhập.</a></label>
 
         </form>
 
@@ -214,7 +190,7 @@
 
   <footer class="container-fluid bg-4 text-center">
   <br>
-  <p> Food Exploria 2017 | &copy All Rights Reserved </p>
+  <p> HUYFOOD 2025 | &copy  </p>
   <br>
   </footer>
 </html>
