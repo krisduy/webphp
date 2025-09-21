1709c1e7ed12731b41126536e6bb35c81f9840e8
krisduy <nguyenthai0871@gmail.com>
2025-09-21 20:09:23 +0700

    sửa trang đăng kí user

--- customersignup.php
+++ customersignup.php
@@ -1,7 +1,7 @@
 <html>
 
   <head>
-    <title> Guest Signup | Food Exploria </title>
+    <title> User Đăng Kí | HUYFOOD </title>
   </head>
 
   <link rel="stylesheet" type = "text/css" href ="css/managersignup.css">
@@ -9,32 +9,6 @@
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-  <body>
-
-  <!--Back to top button..................................................................................-->
-    <button onclick="topFunction()" id="myBtn" title="Go to top">
-      <span class="glyphicon glyphicon-chevron-up"></span>
-    </button>
-  <!--Javacript for back to top button....................................................................-->
-    <script type="text/javascript">
-      window.onscroll = function()
-      {
-        scrollFunction()
-      };
-
-      function scrollFunction(){
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById("myBtn").style.display = "block";
-        } else {
-          document.getElementById("myBtn").style.display = "none";
-        }
-      }
-
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
-    </script>
 
     <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
       <div class="container">
@@ -45,29 +19,29 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Food Exploria</a>
+          <a class="navbar-brand" href="index.php">HUYFOOD</a>
         </div>
 
         <div class="collapse navbar-collapse " id="myNavbar">
           <ul class="nav navbar-nav">
-            <li  ><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About</a></li>
-            <li><a href="contactus.php">Contact Us</a></li>
+            <li  ><a href="index.php">Trang Chủ</a></li>
+            <li><a href="aboutus.php">Về Chúng Tôi</a></li>
+            <li><a href="contactus.php">Liên Hệ</a></li>
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Đăng Kí <span class="caret"></span> </a>
                 <ul class="dropdown-menu">
-              <li> <a href="customersignup.php"> User Sign-up</a></li>
-              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+              <li> <a href="customersignup.php"> User Đăng Kí</a></li>
+              <li> <a href="managersignup.php"> Admin Đăng Kí</a></li>
     
             </ul>
             </li>
 
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Đăng Nhập <span class="caret"></span></a>
               <ul class="dropdown-menu">
-              <li> <a href="customerlogin.php"> User Login</a></li>
-              <li> <a href="managerlogin.php"> Manager Login</a></li>
+              <li> <a href="customerlogin.php"> User Đăng Nhập</a></li>
+              <li> <a href="managerlogin.php"> Admin Đăng Nhập</a></li>
       
             </ul>
             </li>
@@ -79,9 +53,9 @@
 
     <div class="container">
     <div class="jumbotron">
-     <h1>Hi Guest, <br> Welcome to <span class="edit"> FoodExploria </span></h1>
+     <h1>Xin Chào, <br> Chào Mừng Bạn Đến Với <span class="edit"> HUYFOOD </span></h1>
      <br>
-   <p>Get started by creating your account</p>
+   <p>Vui lòng đăng kí để tiếp tục.</p>
     </div>
     </div>
 
@@ -90,7 +64,7 @@
     <div class="container" style="margin-top: 4%; margin-bottom: 2%;">
       <div class="col-md-5 col-md-offset-4">
       <div class="panel panel-primary">
-        <div class="panel-heading"> Create Account </div>
+        <div class="panel-heading"> Tạo tài khoản mới </div>
         <div class="panel-body">
           
         <form role="form" action="customer_registered_success.php" method="POST">
@@ -99,7 +73,7 @@
           <div class="form-group col-xs-12">
             <label for="fullname"><span class="text-danger" style="margin-right: 5px;">*</span> Full Name: </label>
             <div class="input-group">
-              <input class="form-control" id="fullname" type="text" name="fullname" placeholder="Your Full Name" required="" autofocus="">
+              <input class="form-control" id="fullname" type="text" name="fullname" placeholder="Nhập họ và tên bạn" required="" autofocus="">
               <span class="input-group-btn">
                 <label class="btn btn-primary"><span class="glyphicon glyphicon-user" aria-hidden="true"></label>
             </span>
@@ -112,7 +86,7 @@
           <div class="form-group col-xs-12">
             <label for="username"><span class="text-danger" style="margin-right: 5px;">*</span> Username: </label>
             <div class="input-group">
-              <input class="form-control" id="username" type="text" name="username" placeholder="Your Username" required="">
+              <input class="form-control" id="username" type="text" name="username" placeholder="Nhập Username" required="">
               <span class="input-group-btn">
                 <label class="btn btn-primary"><span class="glyphicon glyphicon-user" aria-hidden="true"></label>
             </span>
@@ -125,7 +99,7 @@
           <div class="form-group col-xs-12">
             <label for="email"><span class="text-danger" style="margin-right: 5px;">*</span> Email: </label>
             <div class="input-group">
-              <input class="form-control" id="email" type="email" name="email" placeholder="Email" required="">
+              <input class="form-control" id="email" type="email" name="email" placeholder="Nhập Email" required="">
               <span class="input-group-btn">
                 <label class="btn btn-primary"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></label>
             </span>
@@ -136,9 +110,9 @@
 
         <div class="row">
           <div class="form-group col-xs-12">
-            <label for="contact"><span class="text-danger" style="margin-right: 5px;">*</span> Contact: </label>
+            <label for="contact"><span class="text-danger" style="margin-right: 5px;">*</span> Số điện thoại: </label>
             <div class="input-group">
-              <input class="form-control" id="contact" type="text" name="contact" placeholder="Contact" required="">
+              <input class="form-control" id="contact" type="text" name="contact" placeholder="Nhập số diện thoại" required="">
               <span class="input-group-btn">
                 <label class="btn btn-primary"><span class="glyphicon glyphicon-phone" aria-hidden="true"></span></label>
             </span>
@@ -149,9 +123,9 @@
 
         <div class="row">
           <div class="form-group col-xs-12">
-            <label for="address"><span class="text-danger" style="margin-right: 5px;">*</span> Address: </label>
+            <label for="address"><span class="text-danger" style="margin-right: 5px;">*</span> Địa Chỉ: </label>
             <div class="input-group">
-              <input class="form-control" id="address" type="text" name="address" placeholder="Address" required="">
+              <input class="form-control" id="address" type="text" name="address" placeholder="Địa Chỉ" required="">
               <span class="input-group-btn">
                 <label class="btn btn-primary"><span class="glyphicon glyphicon-home" aria-hidden="true"></label>
             </span>
@@ -164,7 +138,7 @@
           <div class="form-group col-xs-12">
             <label for="password"><span class="text-danger" style="margin-right: 5px;">*</span> Password: </label>
             <div class="input-group">
-              <input class="form-control" id="password" type="password" name="password" placeholder="Password" required="">
+              <input class="form-control" id="password" type="password" name="password" placeholder="Nhập Password" required="">
               <span class="input-group-btn">
                 <label class="btn btn-primary"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></label>
             </span>
@@ -177,12 +151,12 @@
 
         <div class="row">
           <div class="form-group col-xs-4">
-              <button class="btn btn-primary" type="submit">Submit</button>
+              <button class="btn btn-primary" type="submit">Đăng Kí</button>
           </div>
 
         </div>
         <label style="margin-left: 5px;">or</label> <br>
-       <label style="margin-left: 5px;"><a href="customerlogin.php">Have an account? Login.</a></label>
+       <label style="margin-left: 5px;"><a href="customerlogin.php">Đã có tài khoản? Đăng Nhập.</a></label>
 
         </form>
 
@@ -196,7 +170,7 @@
 
   <footer class="container-fluid bg-4 text-center">
   <br>
-  <p> Food Exploria 2017 | &copy All Rights Reserved </p>
+  <p> HUYFOOD 2025 | &copy  </p>
   <br>
   </footer>
 </html>
