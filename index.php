b49e0d0e397fd9bac8d1ee8a17071201e3a310d0
krisduy <nguyenthai0871@gmail.com>
2025-09-21 20:47:25 +0700

    thêm script

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ session_start();
   <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
 
   <link rel="stylesheet" type = "text/css" href ="css/index.css">
-
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  
   <body>
 
 
