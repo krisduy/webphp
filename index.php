6568eca11fe9b79c1528da8170e1cfbaec72832a
krisduy <nguyenthai0871@gmail.com>
2025-09-22 20:55:14 +0700

    p

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ if(isset($_SESSION['login_user1'])){
 
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Xin Chào <?php echo $_SESSION['login_user1']; ?> </a></li>
-            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
-            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+            <li><a href="myrestaurant.php">Trang Quản Lí</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Đăng Xuất </a></li>
           </ul>
 <?php
 }
