c415319996ea2601e0ec97b6a677405a9e5b9082
krisduy <nguyenthai0871@gmail.com>
2025-09-25 01:43:19 +0700

    sửa

--- delete_food_items.php
+++ delete_food_items.php
@@ -19,25 +19,6 @@ if(!isset($login_session)){
 
 <body>
 
-<!--Back to top button-->
-<button onclick="topFunction()" id="myBtn" title="Go to top">
-  <span class="glyphicon glyphicon-chevron-up"></span>
-</button>
-
-<script type="text/javascript">
-  window.onscroll = function() { scrollFunction() };
-  function scrollFunction(){
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById("myBtn").style.display = "block";
-      } else {
-          document.getElementById("myBtn").style.display = "none";
-      }
-  }
-  function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-  }
-</script>
 
 <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
   <div class="container">
