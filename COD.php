ea9cba4f2e34aaa6fef02efd0f449207ecb52ce2
Nguyentecnologyinformation <Nguyentrungduclt2310@gmail.com>
2025-09-25 00:02:16 +0700

    sửa lỗi chữ

--- COD.php
+++ COD.php
@@ -33,7 +33,6 @@ unset($_SESSION["cart"]);
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="index.php">HUYFOOD</a>
-          <a class="navbar-brand" href="index.php">HUYFOOD</a>
         </div>
 
         <div class="collapse navbar-collapse " id="myNavbar">
--- cart.php
+++ cart.php
@@ -31,14 +31,10 @@ header("location: customerlogin.php"); //Redirecting to myrestaurant Page
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="index.php">HUYFOOD</a>
-          <a class="navbar-brand" href="index.php">HUYFOOD</a>
         </div>
 
         <div class="collapse navbar-collapse " id="myNavbar">
           <ul class="nav navbar-nav">
-            <li><a href="index.php">Trang Chủ</a></li>
-            <li><a href="aboutus.php">Về Chúng Tôi</a></li>
-            <li><a href="contactus.php">Liên Hệ</a></li>
             <li><a href="index.php">Trang Chủ</a></li>
             <li><a href="aboutus.php">Về Chúng Tôi</a></li>
             <li><a href="contactus.php">Liên Hệ</a></li>
