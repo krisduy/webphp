dba41019694e7d5894a207736583c7e368608c13
krisduy <nguyenthai0871@gmail.com>
2025-09-25 01:33:00 +0700

    sửa add_food_items1

--- add_food_items1.php
+++ add_food_items1.php
@@ -33,24 +33,6 @@ if (!$success){
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <!--Back to top button-->
-        <button onclick="topFunction()" id="myBtn" title="Go to top">
-            <span class="glyphicon glyphicon-chevron-up"></span>
-        </button>
-        <script type="text/javascript">
-            window.onscroll = function() { scrollFunction() };
-            function scrollFunction(){
-                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                    document.getElementById("myBtn").style.display = "block";
-                } else {
-                    document.getElementById("myBtn").style.display = "none";
-                }
-            }
-            function topFunction() {
-                document.body.scrollTop = 0;
-                document.documentElement.scrollTop = 0;
-            }
-        </script>
 
         <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
           <div class="container">
@@ -72,7 +54,7 @@ if (!$success){
               </ul>
 
               <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $login_session; ?> </a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Xin Chào <?php echo $login_session; ?> </a></li>
                 <li class="active"> <a href="managerlogin.php">Trang Admin</a></li>
                 <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Đăng Xuất </a></li>
               </ul>
@@ -91,7 +73,7 @@ if (!$success){
     </body>
     <footer class="container-fluid bg-4 text-center">
       <br>
-    <p>HUYFOOD 2025 | &copy All Rights Reserved</p>
+    <p>HUYFOOD 2025 | &copy </p>
       <br>
     </footer>
     </html>
