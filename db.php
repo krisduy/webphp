64c745827b4c23b1d1aec716807edb4c7cc7bace
Nguyentecnologyinformation <Nguyentrungduclt2310@gmail.com>
2025-10-18 22:50:42 +0700

    sua-order-detail

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
+$server   = "sql103.infinityfree.com"; // MySQL Host
+$username = "if0_40080644";            // MySQL Username (không có khoảng trắng)
+$password = "huyfood123";              // Mật khẩu DB
+$dbname   = "if0_40080644_huyfood";    // Tên Database
 
-$sever = "sql103.infinityfree.com";
-$username = "if0_40080644 ";
-$password = "huyfood123";
-$dbname = "if0_40080644_huyfood";
-
-$conn =  mysqli_connect($sever, $username, $password, $dbname);
+$conn = mysqli_connect($server, $username, $password, $dbname);
 
 if (!$conn) {
     die("Kết nối thất bại: " . mysqli_connect_error());
 }
+?>
